04f3bc37980f3a64db3fac2e7535e2ba63ddec6b
H-S <taip21401@gmail.com>
2024-03-23 23:03:09 +0200

    layout screen splash screen

---
+++ lib/pages/browser_view/pages/browser_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class browser_view extends StatelessWidget {
+  static const String routeName = "browser_view";
+  const browser_view({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("browser_view"),
+    );
+  }
+}
